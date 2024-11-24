d1a208c776a135652a0b315c1f7c5dda01cc9196
mjwSolver <mwiradinata@student.ciputra.ac.id>
2024-11-24 08:36:40 +0700

    Initial Commit

---
+++ perpuskan/perpuskanApp.swift
@@ -0,0 +1,17 @@
+//
+//  perpuskanApp.swift
+//  perpuskan
+//
+//  Created by Marcell JW on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct perpuskanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ perpuskanTests/perpuskanTests.swift
@@ -0,0 +1,17 @@
+//
+//  perpuskanTests.swift
+//  perpuskanTests
+//
+//  Created by Marcell JW on 24/11/24.
+//
+
+import Testing
+@testable import perpuskan
+
+struct perpuskanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
